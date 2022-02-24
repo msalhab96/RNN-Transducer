a5b8f3be842a62e08eba242ab4ed8ac9c2b7a2c2
Mahmoud Salhab <mahmoud@salhab.work>
2022-02-24 20:56:01 +0200

    hn, cn added to the forward LSTM in PredNet

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 
 class PredNet(nn.Module):
-    """Implements the functionalities of the 
+    """Implements the functionalities of the
     predict network in the model arcticture
 
     Attributes
@@ -17,7 +17,7 @@ class PredNet(nn.Module):
     def __init__(
             self,
             vocab_size: int,
-            emb_dim: int, 
+            emb_dim: int,
             pad_idx: int,
             hidden_size: int,
             n_layers: int,
@@ -26,14 +26,17 @@ class PredNet(nn.Module):
         """Constructs all the necessary attributes
 
         Args:
-            vocab_size (int): The number of vocabularies in the dataset, used for the embedding layer
+            vocab_size (int): The number of vocabularies in the dataset,
+            used for the embedding layer
             emb_dim (int): The embedding layer dimensionality
-            pad_idx (int): the padding index 
+            pad_idx (int): the padding index
             hidden_size (int): The RNN's hidden layer size
             n_layers (int): the number of stacked LSTM layers in the network
             dropout (float): the dropout rate for each RNN layer
         """
         super().__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
         self.emb = nn.Embedding(
             vocab_size, emb_dim, padding_idx=pad_idx
             )
@@ -46,14 +49,30 @@ class PredNet(nn.Module):
         )
 
     def forward(
-            self, 
-            x: Tensor, 
-            hn: Tensor, 
+            self,
+            x: Tensor,
+            hn: Tensor,
             cn: Tensor
             ) -> Tuple[Tensor, Tensor, Tensor]:
+        self.validate_dims(x, hn, cn)
         out = self.emb(x)
-        out, (hn, cn) = self.lstm(out)
+        out, (hn, cn) = self.lstm(out, (hn, cn))
         return out, hn, cn
+
+    def validate_dims(
+            self,
+            x: Tensor,
+            hn: Tensor,
+            cn: Tensor
+            ) -> None:
+        assert hn.shape[0] == self.n_layers, \
+            'The hidden state should match the number of layers'
+        assert hn.shape[2] == self.hidden_size, \
+            'The hidden state should match the hiden size'
+        assert cn.shape[0] == self.n_layers, \
+            'The cell state should match the number of layers'
+        assert cn.shape[2] == self.hidden_size, \
+            'The cell state should match the hiden size'
 
 
 class TransNet(nn.Module):
@@ -62,6 +81,7 @@ class TransNet(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         pass
+
 
 class JoinNet(nn.Module):
     def __init__(self) -> None:
