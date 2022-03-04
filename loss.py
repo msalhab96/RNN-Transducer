cbd86ede029580e722e98fb000c34266ded9e1b1
Mahmoud Salhab <mahmoud@salhab.work>
2022-03-04 15:06:12 +0300

    get_phi_probs and get_chars_probs added

--- loss.py
+++ loss.py
@@ -21,3 +21,13 @@ class Loss(nn.Module):
             self, batch_size: int, n_chars: int, n_phis: int
             ) -> Tensor:
         return torch.zeros(batch_size, n_chars, n_phis)
+
+    def get_phi_probs(self, probs: Tensor, c: int, p: int) -> Tensor:
+        return probs[:, c + p - 1, self.phi_idx]
+
+    def get_chars_probs(
+            self, probs: Tensor, target: Tensor, c: int, p: int
+            ) -> Tensor:
+        all_seqs = probs[:, p + c - 1]
+        result = torch.index_select(all_seqs, dim=-1, index=target[:, c - 1])
+        return result[0, :]
