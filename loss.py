7a06eb83fd49b1ed93b713e0278c32e6b3d80b31
Mahmoud Salhab <mahmoud@salhab.work>
2022-03-04 15:05:31 +0300

    get_score_matrix method added

--- loss.py
+++ loss.py
@@ -1,8 +1,23 @@
+import torch
 import torch.nn as nn
+from torch import Tensor
+
 
 class Loss(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, phi_idx: int, eps: float) -> None:
         super().__init__()
+        self.phi_idx = phi_idx
+        self.eps = eps
 
-    def forward(self):
+    def forward(
+            self,
+            probs: Tensor,
+            target: Tensor,
+            target_lengths: Tensor,
+            ):
         pass
+
+    def get_score_matrix(
+            self, batch_size: int, n_chars: int, n_phis: int
+            ) -> Tensor:
+        return torch.zeros(batch_size, n_chars, n_phis)
